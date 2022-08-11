371363df6b4e92ce7440f94558871c77d3879b88
dooopia <dootopia00@gmail.com>
2022-08-12 02:37:12 +0900

    add jump

--- js/combination.js
+++ js/combination.js
@@ -10,22 +10,22 @@
 */
 
 const combinations = function (arr, selectNumber) {
-    
-    const results = [];
+  
+  const results = [];
 
-    if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
+  if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
 
-    
-    arr.forEach((fixed, index, array ) => {
+  
+  arr.forEach((fixed, index, array ) => {
 
-        const remain = array.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
-        const combination = combinations(remain, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
-        const attached = combination.map((combination) => [fixed, ...combination]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
-        results.push(...attached); // 배열 spread syntax 로 모두 다 push
+      const remain = array.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
+      const combination = combinations(remain, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
+      const attached = combination.map((combination) => [fixed, ...combination]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+      results.push(...attached); // 배열 spread syntax 로 모두 다 push
 
-    });
+  });
 
-    return results; // 결과가 담긴 results를 return
+  return results; // 결과가 담긴 results를 return
 };
 
 const factorization = function(){
