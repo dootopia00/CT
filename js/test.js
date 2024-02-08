fe8cde2dcf4577463fe3c2d6647644741ddee2fc
dootopia <dootopia00@gmail.com>
2024-02-08 23:47:27 +0900

    [feature] test.js add

---
+++ js/test.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    // console.log(Number(input[0]) + Number(input[1]));
+    
+    let a = Number(input[0]);
+    let b = Number(input[1]);
+    console.log("a = " + a);
+    console.log("b = " + b);
+});
