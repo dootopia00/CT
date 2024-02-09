66ba973537c630e1a1fae70952cd32d2fc31594f
dootopia <dootopia00@gmail.com>
2024-02-10 01:01:03 +0900

    [fix] change number

--- js/test.js
+++ js/test.js
@@ -1,18 +1,61 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    // console.log(Number(input[0]) + Number(input[1]));
+function solution(x, y, n) {
     
-    let a = Number(input[0]);
-    let b = Number(input[1]);
-    console.log("a = " + a);    
-    console.log("b = " + b);
-});
+    if(x < 1) return -1;
+    if(y < 1) return -1;
+    
+    let c1=0,c2=0,c3=0;
+    let x1=x, x2=x, x3=x;
+    let y1=y, y2=y, y3=y;
+    let n1=n, n2=n, n3=n;
+    let arr=[];
+    
+    
+    while(1){
+        c1++;
+        if(y1==x1+n1){
+            arr.push(c1);
+            break;            
+        }else{
+            x1=x1+n1;
+        }
+        if(x1>=y1){
+            c1 = -1;
+            break; 
+        }
+    }
+    
+    // 10, 40, 5, 2
+    while(1){
+        c2++;
+        if(y2==2*x2){
+            arr.push(c2);
+            break;            
+        }else{
+            x2=2*x2;
+        }
+        if(x2>=y2){
+            c2 = -1;
+            break; 
+        }
+    }
+    
+    while(1){
+        c3++;
+        if(y3==3*x3){
+            arr.push(c3);
+            break;            
+        }else{
+            x3=3*x3;
+        }
+        if(x3>=y3){
+            c3 = -1;
+            break; 
+        }
+    }
+    
+    if(arr.length === 0) return -1;
+        
+    console.log('arr : ', arr)
+    return Math.min(...arr);
+}
+
